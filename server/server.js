5e44abb76e3049d317c35f0b67913ccf7129e684
unknown <ahmed.chouikh2020@gmail.com>
2025-04-22 19:51:29 +0200

    yte

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 app.use('/api/notification', notificationRoutes);
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on  http:localhost:${PORT}`);
     }
 );
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
